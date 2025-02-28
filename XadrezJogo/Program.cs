2cc842d8f3c4f79aa870fe5479f4bd6d712a47c3
Rafael Adelungue <rafaeladelung@gmail.com>
2025-02-28 13:38:52 -0300

    tratando erros de posição e pequenas noas adições no tabuleiro

--- XadrezJogo/Program.cs
+++ XadrezJogo/Program.cs
@@ -6,14 +6,21 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        TabuleiroJogo tab = new TabuleiroJogo(8, 8);
+        try
+        {
 
-        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 5));
-        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(5, 2));
-        tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(3, 6));
+            TabuleiroJogo tab = new TabuleiroJogo(8, 8);
 
-        Tela.imprimirTabuleiro(tab);
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 5));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(5, 2));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(3, 6));
 
+            Tela.imprimirTabuleiro(tab);
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
         Console.ReadLine();
     }
 }
