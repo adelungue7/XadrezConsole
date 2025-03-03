3a91e7d833bb7f6f2c98ea2194b484f07fadbeba
Rafael Adelungue <rafaeladelung@gmail.com>
2025-03-03 10:35:49 -0300

    Testes iniciais com movimentos possíveis das peças

--- XadrezJogo/Program.cs
+++ XadrezJogo/Program.cs
@@ -18,6 +18,13 @@ internal class Program
                 Console.WriteLine();
                 Console.Write("Origem: ");
                 Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                Console.Clear();
+                bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+
+                Console.WriteLine();
                 Console.Write("Destino: ");
                 Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
--- XadrezJogo/Tabuleiro/Peca.cs
+++ XadrezJogo/Tabuleiro/Peca.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace XadrezJogo.Tabuleiro
 {
-    internal class Peca
+    internal abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -24,5 +24,6 @@ namespace XadrezJogo.Tabuleiro
         {
             qteMovimentos++;
         }
+        public abstract bool[,] movimentosPossiveis();
     }
 }
