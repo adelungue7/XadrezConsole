dba44ffb5690ad6fb052503fd2b5861d37fd90c0
Rafael Adelungue <rafaeladelung@gmail.com>
2025-03-06 13:42:26 -0300

    jogada especial promoção, projeto finalizado

--- XadrezJogo/Xadrez/PartidaDeXadrez.cs
+++ XadrezJogo/Xadrez/PartidaDeXadrez.cs
@@ -142,6 +142,22 @@ namespace XadrezJogo.Xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+
+            Peca p = tab.peca(destino);
+            //jogadaEspecial Promoção
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0) || 
+                    (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -160,7 +176,7 @@ namespace XadrezJogo.Xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
             //jogadaEspecial En Passant
 
             if(p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha +2))
